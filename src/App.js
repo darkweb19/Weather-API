2db6f9d437698a67e7f790d3610e279fcc3006dc
darkweb19 <sujanstha3220@gmail.com>
2022-11-25 18:53:09 +0545

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import TempApp from './components/tempapp';
+
+function App() {
+  return (
+    
+    <div>
+      <TempApp/>
+    </div>
+  );
+}
+
+export default App;
